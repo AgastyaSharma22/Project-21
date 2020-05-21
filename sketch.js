ca909e710fae0f636167999edcac094be4ba8001
AgastyaSharma22 <65062274+AgastyaSharma22@users.noreply.github.com>
2020-05-21 19:33:19 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,4 +29,12 @@ function draw() {
  
   drawSprites();
 }
-
+function hasCollided(lbullet,lwall)
+{
+  bulletRightEdge=lbullet.x+lbullet.width;
+  wallLeftEdge=lwall.x;
+  if(bulletRightEdge>=wallLeftEdge){
+    return true;
+  }
+  return false;
+}
